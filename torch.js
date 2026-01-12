40970014369a9e1d48355aa3dbc5e7489f29a9b4
6Morpheus6 <wanton-decay@gmx.net>
2026-01-12 05:44:10 +0100

    update

--- torch.js
+++ torch.js
@@ -44,7 +44,7 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "uv pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1",
+          "uv pip install wheel torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1",
           "uv pip install xformers==0.0.20 --no-build-isolation"
         ]
       }
